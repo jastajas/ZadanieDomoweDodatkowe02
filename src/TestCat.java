093c8b57a43b148b86b0f6a03a2f1a0cf8dd2258
jackS <jastajas@gamil.com>
2018-01-20 15:04:56 +0100

    Nowa klasa testowa kot (main)

---
+++ src/TestCat.java
@@ -0,0 +1,13 @@
+public class TestCat {
+    public static void main(String[] args) {
+        char char1 = (char) 75;
+        char char2 = (char) 79;
+        char char3 = (char) 84;
+
+
+        System.out.print(char1);
+        System.out.print(char2);
+        System.out.print(char3);
+
+    }
+}
